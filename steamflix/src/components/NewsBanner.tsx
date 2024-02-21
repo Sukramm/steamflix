6bef0c6de1e8348ef5c879be954640bac8580c95
Markus Veem <markus.veem@gmail.com>
2024-02-21 20:19:38 +0200

    some work on store mainBanner

--- steamflix/src/components/NewsBanner.tsx
+++ steamflix/src/components/NewsBanner.tsx
@@ -1,9 +1,46 @@
-import testEvent from "../assets/pics/events/testEvent.png";
+import { Link } from "react-router-dom";
 import "../styles/Store/newsBanner.scss";
 
 function NewsBanner() {
+  const navMenuItems = [
+    {
+      item: "Category",
+      link: "#",
+      subItems: ["Action", "RPG", "F2P", "DEMO", "RTS", "Racing", "Co-op"],
+    },
+    {
+      item: "Point Shop",
+      link: "/store/pointsShop",
+      subItems: null,
+    },
+    {
+      item: "News",
+      link: "/store/news",
+      subItems: null,
+    },
+    {
+      item: "Wishlist",
+      link: "/store/Wishlist",
+      subItems: null,
+    },
+  ];
   return (
-    <div className="newsBanner">{testEvent && <img src={testEvent}></img>}</div>
+    <div className="newsBanner">
+      <div className="mainBanner">
+        <div className="navMenu">
+          <div className="navMenuWrap">
+            <div className="navItems">
+              {navMenuItems.map((item) => (
+                <div className={`navitem ${item.item}`}>
+                  <Link to={item.link}>{item.item}</Link>
+                </div>
+              ))}
+              <div className="navitem">Search</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
